8b9e109012f90e221dc469f50a92cd08837403cc
Nick Spaargaren <>
2020-04-06 12:38:06 +0200

    first commit

---
+++ Water drinken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Water drinken
+//
+//  Created by Nick Spaargaren on 4/6/20.
+//  Copyright © 2020 Nick Spaargaren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Water drinkenTests/Water_drinkenTests.swift
@@ -0,0 +1,34 @@
+//
+//  Water_drinkenTests.swift
+//  Water drinkenTests
+//
+//  Created by Nick Spaargaren on 4/6/20.
+//  Copyright © 2020 Nick Spaargaren. All rights reserved.
+//
+
+import XCTest
+@testable import Water_drinken
+
+class Water_drinkenTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
